c1bd1875f75007d10e910f77b6eb5672edddc7a7
Jimmy Bates <jbates.productions@gmail.com>
2023-12-08 00:19:25 -0800

    day3 done

--- day3/gear_ratios.py
+++ day3/gear_ratios.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from os import walk
 from pprint import pprint
 
 
@@ -21,6 +22,7 @@ mat_list = []
 num_list = []
 sym_list = []
 sum_list = []
+star_list = []
 
 with open("data.txt", "r", encoding="utf-8") as f:
     for line in f:
@@ -52,30 +54,49 @@ for i, row in enumerate(mat_list):
             num = 0
         if symbol == "." or symbol == " " or symbol == "\n":
             continue
+        if symbol == "*":
+            star_list.append(SymPT(x=j, y=i))
         if add_int:
             sym_list[i].append(SymPT(x=j, y=i))
 
+# part2
+mult_sum = 0
+for star_obj in star_list:
+    alt_num_list = list()
+    for num_obj in num_list:
+        if (
+            -1 <= num_obj.y - star_obj.y <= 1
+            and num_obj.x - 1 <= star_obj.x < num_obj.x + num_obj.width + 1
+        ):
+            alt_num_list.append(num_obj)
+    # print(star_obj)
+    if not len(alt_num_list) == 2:
+        continue
+    mult_sum += alt_num_list[0].num * alt_num_list[1].num
+print(mult_sum)
+
+# part1
 for num_obj in num_list:
     touching = False
     if num_obj.y > 0:
         for sym_obj in sym_list[num_obj.y - 1]:
-            if num_obj.x - 1 <= sym_obj.x <= num_obj.x + num_obj.width + 1:
+            if num_obj.x - 1 <= sym_obj.x < num_obj.x + num_obj.width + 1:
                 touching = True
                 break
     if not touching and num_obj.y < len(sym_list) - 1:
         for sym_obj in sym_list[num_obj.y + 1]:
-            if num_obj.x - 1 <= sym_obj.x <= num_obj.x + num_obj.width + 1:
+            if num_obj.x - 1 <= sym_obj.x < num_obj.x + num_obj.width + 1:
                 touching = True
                 break
     if not touching:
         for sym_obj in sym_list[num_obj.y]:
-            if num_obj.x - 1 <= sym_obj.x <= num_obj.x + num_obj.width + 1:
+            if num_obj.x - 1 <= sym_obj.x < num_obj.x + num_obj.width + 1:
                 touching = True
                 break
     if touching:
         sum_list.append(num_obj)
-    else:
-        print(num_obj.num)
+    # else:
+    #     print(num_obj.num)
 
-pprint(sum_list)
+# pprint(sum_list)
 print(sum([x.num for x in sum_list]))
